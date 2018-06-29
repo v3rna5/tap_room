df21c24be7407f9549e8ddfb06aeb03517fd2a93
verna <vernajs@gmail.com>
2018-06-29 10:28:44 -0700

    Fix naming

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 function Header(){
   return (
     <div>
-      <h1>Help Queue</h1>
-      <Link to="/">Home</Link> | <Link to="/newtap">Create Tap</Link>
+      <h1>Tap Room</h1>
+      <Link to="/">Home</Link> | <Link to="/newtap">Add Tap</Link>
     </div>
   );
 }
