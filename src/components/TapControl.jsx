37c8a57e13aa64ca1f843f1ebc4217d41b763afb
verna <vernajs@gmail.com>
2018-07-06 09:48:05 -0700

    Add Control Component

--- src/components/TapControl.jsx
+++ src/components/TapControl.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import ConfirmationQuestions from './ConfirmationQuestions';
+import TapForm from './TapForm';
+import PropTypes from 'prop-types';
 
 class TapControl extends React.Component {
 
@@ -7,15 +10,30 @@ class TapControl extends React.Component {
     this.state = {
       formVisibleOnPage: false
     };
+    this.handleTroubleshootingConfirmation = this.handleTroubleshootingConfirmation.bind(this);
+  }
+
+  handleTroubleshootingConfirmation(){
+    this.setState({formVisibleOnPage: true});
   }
 
   render(){
+    let currentlyVisibleContent = null;
+    if (this.state.formVisibleOnPage){
+      currentlyVisibleContent = <TapForm onNewTapCreation={this.props.onNewTapCreation}/>;
+    } else {
+      currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation}/>;
+    }
     return (
       <div>
-        <p>This is the NewTicketControl component!</p>
+        {currentlyVisibleContent}
       </div>
     );
   }
 }
+
+TapControl.propTypes = {
+  onNewTapCreation: PropTypes.func
+};
 
 export default TapControl;
