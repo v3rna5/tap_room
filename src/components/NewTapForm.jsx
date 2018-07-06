cbc2e85f90a40cb140215dd267e6973ef50efe76
verna <vernajs@gmail.com>
2018-07-06 14:00:43 -0700

    Updated Form

--- src/components/NewTapForm.jsx
+++ src/components/NewTapForm.jsx
@@ -6,13 +6,19 @@ function NewTapForm(props){
   let _names = null;
   let _brewer = null;
   let _description = null;
+  let _abv = null;
+  let _price = null;
+  let _remaining = null;
 
   function handleNewTapFormSubmission(event) {
     event.preventDefault();
-    props.onNewTapCreation({names: _names.value, brewer: _brewer.value, description: _description.value, timeOpen: new Moment()});
+    props.onNewTapCreation({names: _names.value, brewer: _brewer.value, description: _description.value, abv: _abv.value, price: _price.value, remaining: _remaining.value, timeOpen: new Moment()});
     _names.value = '';
     _brewer.value = '';
     _description.value = '';
+    _abv.value = '';
+    _price.value = '';
+    _remaining.value = '';
   }
 
   return (
@@ -28,11 +34,27 @@ function NewTapForm(props){
           id='brewer'
           placeholder='Brewer'
           ref={(input) => {_brewer = input;}}/>
+
         <textarea
           id='description'
           placeholder='Tap Description'
           ref={(textarea) => {_description = textarea;}}/>
-        <button type='submit'>Help!</button>
+          <input
+            type='text'
+            id='abv'
+            placeholder='Abv'
+            ref={(input) => {_abv = input;}}/>
+            <input
+              type='text'
+              id='price'
+              placeholder='Price'
+              ref={(input) => {_price = input;}}/>
+              <input
+                type='text'
+                id='remaining'
+                placeholder='Remaining'
+                ref={(input) => {_remaining = input;}}/>
+              <button type='submit'>Add!</button>
       </form>
     </div>
   );
